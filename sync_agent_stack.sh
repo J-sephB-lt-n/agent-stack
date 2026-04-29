9e98ba3b3056cb23ec09c7103a1ff3f94b202cec
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2026-04-29 21:14:57 +0200

    feat(sync_agent_stack.sh): commands now copy correctly to pi agent

--- sync_agent_stack.sh
+++ sync_agent_stack.sh
@@ -32,6 +32,36 @@ if [ -n "${OPENCODE_GLOBAL_CONFIG_DIR}" ]; then
   done
 fi
 
+if [ -n "${PI_AGENT_GLOBAL_CONFIG_DIR}" ]; then
+  # --- AGENTS --- #
+  # find ./agents -name '*.md' -type f | while IFS= read -r filepath; do
+  #   dest_filename=$(basename "$filepath")
+  #   dest_filepath="$OPENCODE_GLOBAL_CONFIG_DIR/agents/$dest_filename"
+  #   set -x
+  #   cp "$filepath" "$dest_filepath"
+  #   sed -i -E '/\s\#\s*(cursor-only|pi-only)\s*$/d' "$dest_filepath"
+  #   set +x
+  # done
+
+  # --- COMMANDS --- #
+  find ./commands -name '*.md' -type f | while IFS= read -r filepath; do
+    dest_filename=$(basename "$filepath")
+    dest_filepath="$PI_AGENT_GLOBAL_CONFIG_DIR/agent/prompts/$dest_filename"
+    set -x
+    cp "$filepath" "$dest_filepath"
+    set +x
+  done
+
+  # --- SKILLS --- #
+  # find ./skills -name '*.md' -type f | while IFS= read -r filepath; do
+  #   skill_dir=$(dirname "$filepath")
+  #   skill_name=$(basename "$skill_dir")
+  #   set -x
+  #   cp -r "$skill_dir/." "$OPENCODE_GLOBAL_CONFIG_DIR/skills/$skill_name/"
+  #   set +x
+  # done
+fi
+
 for CONFIG_DIR in "$CURSOR_GLOBAL_CONFIG_DIR" "$CURSOR_WSL_GLOBAL_CONFIG_DIR"; do
   if [ -n "${CONFIG_DIR}" ]; then
     # --- AGENTS --- #
